37a481564a2cd721a3dab4fa1b5eb6f10f080175
Kevin Galligan <kgalligan@gmail.com>
2015-05-23 00:25:38 -0400

    Before refactor

---
+++ src/main/java/com/j256/ormlite/field/DataType.java
@@ -0,0 +1,207 @@
+package com.j256.ormlite.field;
+
+import com.j256.ormlite.field.types.*;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * Data type enumeration that corresponds to a {@link DataPersister}.
+ * 
+ * @author graywatson
+ */
+public enum DataType {
+
+	/**
+	 * Persists the {@link String} Java class.
+	 */
+	STRING(StringType.getSingleton()),
+	/**
+	 * Persists the {@link String} Java class.
+	 */
+	LONG_STRING(LongStringType.getSingleton()),
+	/**
+	 * Persists the {@link String} Java class as an array of bytes. By default this will use {@link #STRING} so you will
+	 * need to specify this using {@link DatabaseField#dataType()}.
+	 */
+	STRING_BYTES(StringBytesType.getSingleton()),
+	/**
+	 * Persists the boolean Java primitive.
+	 */
+	BOOLEAN(BooleanType.getSingleton()),
+	/**
+	 * Persists the {@link Boolean} object Java class.
+	 */
+	BOOLEAN_OBJ(BooleanObjectType.getSingleton()),
+	/**
+	 * Persists the boolean Java primitive as a character in the database.
+	 */
+	BOOLEAN_CHAR(BooleanCharType.getSingleton()),
+	/**
+	 * Persists the boolean Java primitive as an integer in the database.
+	 */
+	BOOLEAN_INTEGER(BooleanIntegerType.getSingleton()),
+	/**
+	 * Persists the {@link java.util.Date} Java class.
+	 * 
+	 * <p>
+	 * NOTE: This is <i>not</i> the same as the {@link java.sql.Date} class.
+	 * </p>
+	 */
+	DATE(DateType.getSingleton()),
+
+	/**
+	 * Persists the {@link java.util.Date} Java class as long milliseconds since epoch. By default this will use
+	 * {@link #DATE} so you will need to specify this using {@link DatabaseField#dataType()}.
+	 * 
+	 * <p>
+	 * NOTE: This is <i>not</i> the same as the {@link java.sql.Date} class.
+	 * </p>
+	 */
+	DATE_LONG(DateLongType.getSingleton()),
+	/**
+	 * Persists the {@link java.util.Date} Java class as a string of a format. By default this will use {@link #DATE} so
+	 * you will need to specify this using {@link DatabaseField#dataType()}.
+	 * 
+	 * <p>
+	 * NOTE: This is <i>not</i> the same as the {@link java.sql.Date} class.
+	 * </p>
+	 * 
+	 * <p>
+	 * <b>WARNING:</b> Because of SimpleDateFormat not being reentrant, this has to do some synchronization with every
+	 * data in/out unfortunately.
+	 * </p>
+	 */
+	DATE_STRING(DateStringType.getSingleton()),
+	/**
+	 * Persists the char primitive.
+	 */
+	CHAR(CharType.getSingleton()),
+	/**
+	 * Persists the {@link Character} object Java class.
+	 */
+	CHAR_OBJ(CharacterObjectType.getSingleton()),
+	/**
+	 * Persists the byte primitive.
+	 */
+	BYTE(ByteType.getSingleton()),
+	/**
+	 * Persists the byte[] array type. Because of some backwards compatibility issues, you will need to specify this
+	 * using {@link DatabaseField#dataType()}. It won't be detected automatically.
+	 */
+	BYTE_ARRAY(ByteArrayType.getSingleton()),
+	/**
+	 * Persists the {@link Byte} object Java class.
+	 */
+	BYTE_OBJ(ByteObjectType.getSingleton()),
+	/**
+	 * Persists the short primitive.
+	 */
+	SHORT(ShortType.getSingleton()),
+	/**
+	 * Persists the {@link Short} object Java class.
+	 */
+	SHORT_OBJ(ShortObjectType.getSingleton()),
+	/**
+	 * Persists the int primitive.
+	 */
+	INTEGER(IntType.getSingleton()),
+	/**
+	 * Persists the {@link Integer} object Java class.
+	 */
+	INTEGER_OBJ(IntegerObjectType.getSingleton()),
+	/**
+	 * Persists the long primitive.
+	 */
+	LONG(LongType.getSingleton()),
+	/**
+	 * Persists the {@link Long} object Java class.
+	 */
+	LONG_OBJ(LongObjectType.getSingleton()),
+	/**
+	 * Persists the float primitive.
+	 */
+	FLOAT(FloatType.getSingleton()),
+	/**
+	 * Persists the {@link Float} object Java class.
+	 */
+	FLOAT_OBJ(FloatObjectType.getSingleton()),
+	/**
+	 * Persists the double primitive.
+	 */
+	DOUBLE(DoubleType.getSingleton()),
+	/**
+	 * Persists the {@link Double} object Java class.
+	 */
+	DOUBLE_OBJ(DoubleObjectType.getSingleton()),
+	/**
+	 * Persists an unknown Java Object that is serializable. Because of some backwards and forwards compatibility
+	 * concerns, you will need to specify this using {@link DatabaseField#dataType()}. It won't be detected
+	 * automatically.
+	 */
+	SERIALIZABLE(SerializableType.getSingleton()),
+	/**
+	 * Persists an Enum Java class as its string value. You can also specify the {@link #ENUM_INTEGER} as the type.
+	 */
+	ENUM_STRING(EnumStringType.getSingleton()),
+	/**
+	 * Persists an Enum Java class as its ordinal integer value. You can also specify the {@link #ENUM_STRING} as the
+	 * type.
+	 */
+	ENUM_INTEGER(EnumIntegerType.getSingleton()),
+	/**
+	 * Persists the {@link java.util.UUID} Java class.
+	 */
+	UUID(UuidType.getSingleton()),
+	/**
+	 * Persists the {@link java.util.UUID} Java class as a native UUID column which is only supported by a couple of
+	 * database types.
+	 */
+	UUID_NATIVE(UuidType.getSingleton()),
+	/**
+	 * Persists the {@link BigInteger} Java class.
+	 */
+	BIG_INTEGER(BigIntegerType.getSingleton()),
+	/**
+	 * Persists the {@link BigDecimal} Java class as a String.
+	 */
+	BIG_DECIMAL(BigDecimalStringType.getSingleton()),
+	/**
+	 * Persists the {@link BigDecimal} Java class as a SQL NUMERIC.
+	 */
+	BIG_DECIMAL_NUMERIC(BigDecimalNumericType.getSingleton()),
+	/**
+	 * Persists the org.joda.time.DateTime type with reflection since we don't want to add the dependency. Because this
+	 * class uses reflection, you have to specify this using {@link DatabaseField#dataType()}. It won't be detected
+	 * automatically.
+	 */
+	DATE_TIME(DateTimeType.getSingleton()),
+	/**
+	 * Persists the {@link java.sql.Date} Java class.
+	 * 
+	 * <p>
+	 * NOTE: If you want to use the {@link java.util.Date} class then use {@link #DATE} which is recommended instead.
+	 * </p>
+	 */
+	SQL_DATE(SqlDateType.getSingleton()),
+	/**
+	 * Persists the {@link java.sql.Timestamp} Java class. The {@link #DATE} type is recommended instead.
+	 */
+	TIME_STAMP(TimeStampType.getSingleton()),
+	/**
+	 * Marker for fields that are unknown.
+	 */
+	UNKNOWN(null),
+	// end
+	;
+
+	private final DataPersister dataPersister;
+
+	private DataType(DataPersister dataPersister) {
+		this.dataPersister = dataPersister;
+	}
+
+	public DataPersister getDataPersister() {
+		return dataPersister;
+	}
+}
---
+++ src/main/java/com/j256/ormlite/field/types/BaseDataType.java
@@ -0,0 +1,180 @@
+package com.j256.ormlite.field.types;
+
+import com.j256.ormlite.field.BaseFieldConverter;
+import com.j256.ormlite.field.DataPersister;
+import com.j256.ormlite.field.FieldType;
+import com.j256.ormlite.field.SqlType;
+import com.j256.ormlite.support.DatabaseResults;
+
+import java.lang.reflect.Field;
+import java.sql.SQLException;
+
+/**
+ * Base data type that defines the default persistance methods for the various data types.
+ * 
+ * <p>
+ * Here's a good page about the <a href="http://docs.codehaus.org/display/CASTOR/Type+Mapping" >mapping for a number of
+ * database types</a>:
+ * </p>
+ * 
+ * <p>
+ * <b>NOTE:</b> If you are creating your own custom database persister, you probably will need to override the
+ * {@link BaseFieldConverter#sqlArgToJava(FieldType, Object, int)} method as well which converts from a SQL data to
+ * java.
+ * </p>
+ * 
+ * @author graywatson
+ */
+public abstract class BaseDataType extends BaseFieldConverter implements DataPersister {
+
+	private final static Class<?>[] NO_CLASSES = new Class<?>[0];
+
+	/**
+	 * Type of the data as it is persisted in SQL-land. For example, if you are storing a DateTime, you might consider
+	 * this to be a {@link SqlType#LONG} if you are storing it as epoche milliseconds.
+	 */
+	private final SqlType sqlType;
+	private final Class<?>[] classes;
+
+	/**
+	 * @param sqlType
+	 *            Type of the class as it is persisted in the databases.
+	 * @param classes
+	 *            Associated classes for this type. These should be specified if you want this type to be always used
+	 *            for these Java classes. If this is a custom persister then this array should be empty.
+	 */
+	public BaseDataType(SqlType sqlType, Class<?>[] classes) {
+		this.sqlType = sqlType;
+		this.classes = classes;
+	}
+
+	/**
+	 * @param sqlType
+	 *            Type of the class as it is persisted in the databases.
+	 */
+	public BaseDataType(SqlType sqlType) {
+		this.sqlType = sqlType;
+		this.classes = NO_CLASSES;
+	}
+
+	public abstract Object parseDefaultString(FieldType fieldType, String defaultStr) throws SQLException;
+
+	public abstract Object resultToSqlArg(FieldType fieldType, DatabaseResults results, int columnPos)
+			throws SQLException;
+
+	public boolean isValidForField(Field field) {
+		if (classes.length == 0) {
+			// we can't figure out the types so we just say it is valid
+			return true;
+		}
+		for (Class<?> clazz : classes) {
+			if (clazz.isAssignableFrom(field.getType())) {
+				return true;
+			}
+		}
+		// if classes are specified and one of them should match
+		return false;
+	}
+
+	public Class<?> getPrimaryClass() {
+		if (classes.length == 0) {
+			return null;
+		} else {
+			return classes[0];
+		}
+	}
+
+	/**
+	 * @throws SQLException
+	 *             If there are problems creating the config object. Needed for subclasses.
+	 */
+	public Object makeConfigObject(FieldType fieldType) throws SQLException {
+		return null;
+	}
+
+	public SqlType getSqlType() {
+		return sqlType;
+	}
+
+	public Class<?>[] getAssociatedClasses() {
+		return classes;
+	}
+
+	public String[] getAssociatedClassNames() {
+		return null;
+	}
+
+	public Object convertIdNumber(Number number) {
+		// by default the type cannot convert an id number
+		return null;
+	}
+
+	public boolean isValidGeneratedType() {
+		return false;
+	}
+
+	public boolean isEscapedDefaultValue() {
+		// default is to not escape the type if it is a number
+		return isEscapedValue();
+	}
+
+	public boolean isEscapedValue() {
+		return true;
+	}
+
+	public boolean isPrimitive() {
+		return false;
+	}
+
+	public boolean isComparable() {
+		return true;
+	}
+
+	public boolean isAppropriateId() {
+		return true;
+	}
+
+	public boolean isArgumentHolderRequired() {
+		return false;
+	}
+
+	public boolean isSelfGeneratedId() {
+		return false;
+	}
+
+	public Object generateId() {
+		throw new IllegalStateException("Should not have tried to generate this type");
+	}
+
+	public int getDefaultWidth() {
+		return 0;
+	}
+
+	public boolean dataIsEqual(Object fieldObj1, Object fieldObj2) {
+		if (fieldObj1 == null) {
+			return (fieldObj2 == null);
+		} else if (fieldObj2 == null) {
+			return false;
+		} else {
+			return fieldObj1.equals(fieldObj2);
+		}
+	}
+
+	public boolean isValidForVersion() {
+		return false;
+	}
+
+	/**
+	 * Move the current-value to the next value. Used for the version field.
+	 * 
+	 * @throws SQLException
+	 *             For sub-classes.
+	 */
+	public Object moveToNextValue(Object currentValue) throws SQLException {
+		return null;
+	}
+
+	public Object resultStringToJava(FieldType fieldType, String stringValue, int columnPos) throws SQLException {
+		return sqlArgToJava(fieldType, parseDefaultString(fieldType, stringValue), columnPos);
+	}
+}
