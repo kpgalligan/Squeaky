58c21d1bd6d3fc7e703d49ab6c0d34ecd9524dd8
Kevin Galligan <kgalligan@gmail.com>
2015-07-18 01:57:32 -0400

    native generated ids

---
+++ src/main/java/com/j256/ormlite/field/ForeignCollectionField.java
@@ -0,0 +1,19 @@
+package com.j256.ormlite.field;
+
+/**
+ * Created by kgalligan on 7/12/15.
+ */
+public @interface ForeignCollectionField
+{
+	public static final int DEFAULT_MAX_EAGER_LEVEL = 1;
+
+	boolean eager() default false;
+
+	int maxEagerLevel() default DEFAULT_MAX_EAGER_LEVEL;
+
+	String orderColumnName() default "";
+
+	boolean orderAscending() default true;
+
+	String foreignFieldName() default "";
+}
