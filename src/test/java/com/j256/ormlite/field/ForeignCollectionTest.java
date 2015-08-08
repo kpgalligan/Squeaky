e7df11816bda50b9b9837c96b43b625b9c540b0c
Kevin Galligan <kgalligan@gmail.com>
2015-08-08 06:27:30 -0400

    Foreign collections order by test

--- src/test/java/com/j256/ormlite/field/ForeignCollectionTest.java
+++ src/test/java/com/j256/ormlite/field/ForeignCollectionTest.java
@@ -5,14 +5,14 @@ import com.j256.ormlite.android.squeaky.Dao;
 import com.j256.ormlite.field.types.BaseTypeTest;
 import com.j256.ormlite.table.DatabaseTable;
 import org.apache.commons.lang3.ObjectUtils;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -116,6 +116,44 @@ public class ForeignCollectionTest extends BaseTypeTest
 
 		ParentString parentDb = parentDao.queryForAll().get(0);
 //		parentDao.fillForeignCollection(parentDb, "children");
+
+		assertTrue(parentDb.children.equals(children));
+	}
+
+	@Test
+	public void testForeignCollectionOrder() throws Exception
+	{
+		Dao<ParentOrder, Integer> parentDao = helper.getDao(ParentOrder.class);
+
+		ParentOrder parent = new ParentOrder();
+		parent.name = "test";
+		parentDao.create(parent);
+
+		Dao<ChildOrder,Integer> childDao = helper.getDao(ChildOrder.class);
+		Random random = new Random();
+		List<ChildOrder> children = new ArrayList<ChildOrder>();
+
+		for(int i=0; i<20; i++)
+		{
+			ChildOrder child = new ChildOrder();
+			child.asdf = "Hello "+ (100-i);
+			child.parent = parent;
+			childDao.create(child);
+			children.add(child);
+		}
+
+		ParentOrder parentDb = parentDao.queryForAll().get(0);
+
+		Assert.assertFalse(parentDb.children.equals(children));
+
+		Collections.sort(children, new Comparator<ChildOrder>()
+		{
+			@Override
+			public int compare(ChildOrder o1, ChildOrder o2)
+			{
+				return o1.asdf.compareTo(o2.asdf);
+			}
+		});
 
 		assertTrue(parentDb.children.equals(children));
 	}
@@ -264,6 +302,54 @@ public class ForeignCollectionTest extends BaseTypeTest
 		}
 	}
 
+	@DatabaseTable
+	protected static class ParentOrder
+	{
+		@DatabaseField(generatedId = true)
+		int id;
+
+		@DatabaseField
+		String name;
+
+		@ForeignCollectionField(foreignFieldName = "parent", eager = true, orderBy = "asdf")
+		List<ChildOrder> children;
+	}
+
+	@DatabaseTable
+	protected static class ChildOrder {
+		@DatabaseField(generatedId = true)
+		int id;
+
+		@DatabaseField
+		String asdf;
+
+		@DatabaseField(foreign = true)
+		ParentOrder parent;
+
+		@Override
+		public boolean equals(Object o)
+		{
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+
+			ChildOrder child = (ChildOrder) o;
+
+			if (id != child.id) return false;
+			if (asdf != null ? !asdf.equals(child.asdf) : child.asdf != null) return false;
+			return !(parent != null ? !(parent.id == child.parent.id) : child.parent != null);
+
+		}
+
+		@Override
+		public int hashCode()
+		{
+			int result = id;
+			result = 31 * result + (asdf != null ? asdf.hashCode() : 0);
+			result = 31 * result + (parent != null ? parent.hashCode() : 0);
+			return result;
+		}
+	}
+
 	private SimpleHelper getHelper()
 	{
 		return createHelper(
@@ -272,7 +358,9 @@ public class ForeignCollectionTest extends BaseTypeTest
 				ChildLazy.class,
 				ParentLazy.class,
 				ChildString.class,
-				ParentString.class
+				ParentString.class,
+				ChildOrder.class,
+				ParentOrder.class
 		);
 	}
 }
